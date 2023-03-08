7d998da055e074410fea6aeb78dd08d076343786
Luu Phuc <luuphuc6297@gmail.com>
2023-03-08 13:18:25 +0700

    change title

--- src/components/common/Page.js
+++ src/components/common/Page.js
@@ -1,33 +1,35 @@
-import PropTypes from "prop-types";
-import { Helmet } from "react-helmet-async";
-import { forwardRef } from "react";
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet-async';
+import { forwardRef } from 'react';
 // @mui
-import { Box } from "@mui/material";
+import { Box } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-const Page = forwardRef(({ children, title = "", meta, ...other }, ref) => (
-  <>
-    <Helmet>
-      <title>{`${title} | GateKeeper`}</title>
-      {meta}
-    </Helmet>
+const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
+    <>
+        <Helmet>
+            <title>{`${title} | SUI`}</title>
+            {meta}
+        </Helmet>
 
-    <Box
-      sx={{
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 100%",
-      }}
-      ref={ref} {...other}>
-      {children}
-    </Box>
-  </>
+        <Box
+            sx={{
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '100% 100%',
+            }}
+            ref={ref}
+            {...other}
+        >
+            {children}
+        </Box>
+    </>
 ));
 
 Page.propTypes = {
-  children: PropTypes.node.isRequired,
-  title: PropTypes.string,
-  meta: PropTypes.node,
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string,
+    meta: PropTypes.node,
 };
 
 export default Page;
