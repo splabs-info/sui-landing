c3a764f3b57cb1e8f267660ddd25b50d91d4e957
Petrus Sĩ Quí <petersiqui@gmail.com>
2023-06-15 13:52:00 +0700

    -- layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Footer from './FooterSection';
 import HeaderHome from './HeaderSection';
 
 const RootStyle = styled('div')({
-    background: '#121A27',
+    background: 'linear-gradient(150deg, #0C202D 0%, #050C13 100%)',
 });
 const MainStyle = styled('div')(({ theme }) => ({
     paddingTop: '3rem',
