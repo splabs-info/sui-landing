c90e36715a9d64ff0981c0c60e471f703d2536ad
Luu Phuc <luuphuc6297@gmail.com>
2023-07-20 17:40:32 +0700

    handle-route

--- src/routes.js
+++ src/routes.js
@@ -43,14 +43,14 @@ export default function Router() {
             </Suspense>
           ),
         },
-        // {
-        //   path: 'ido-launchpad/round',
-        //   element: (
-        //     <Suspense>
-        //       <IDORound />
-        //     </Suspense>
-        //   ),
-        // },
+        {
+          path: 'ido-launchpad/round',
+          element: (
+            <Suspense>
+              <IDORound />
+            </Suspense>
+          ),
+        },
         {
           path: 'ido-launchpad/sua',
           element: (
