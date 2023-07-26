3456af8dc887878df680a3bc03a1875ae6f3316b
Riley <realbewhy@gmail.com>
2023-07-26 17:01:24 +0700

    #100 -- interval price 2s

--- src/services/price.js
+++ src/services/price.js
@@ -1,15 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
 import { authApis } from 'apis/auth';
+import React from 'react';
 
 const priceKeys = {
-    all: () => ['price'],
-    price: () => [...priceKeys.all(), 'price'],
+  all: () => ['price'],
+  price: () => [...priceKeys.all(), 'price'],
 };
 
 export const useGetPrice = () => {
-    const { data, ...other } = useQuery(priceKeys.price(), () => authApis.getPrice())
+  //   const { data, ...other } = useQuery(priceKeys.price(), () => authApis.getPrice());
+  const [data, setData] = React.useState(0);
 
-    return {
-        price: data
-    }
-}
+  const syncData = async () => {
+    const data = await authApis.getPrice();
+    console.log(data);
+    setData(data);
+  };
+
+  React.useEffect(() => {
+    syncData();
+    const interval = setInterval(() => {
+      syncData();
+    }, 20000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return {
+    price: data,
+  };
+};
