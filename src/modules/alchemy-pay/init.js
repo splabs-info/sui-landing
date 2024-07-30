0fdfceea6ab6d4462382574d1e214bb39fd06f79
Riley <realbewhy@gmail.com>
2024-07-30 13:07:02 +0700

    -- update

--- src/modules/alchemy-pay/init.js
+++ src/modules/alchemy-pay/init.js
@@ -1,8 +1,6 @@
 export const config = {
-  appId: process.env.REACT_APP_ALCHEMY_PAY_APP_ID ? process.env.REACT_APP_ALCHEMY_PAY_APP_ID : '2713VQ03Icjd3n4E',
-  appSecret: process.env.REACT_APP_ALCHEMY_PAY_APP_SECRET
-    ? process.env.REACT_APP_ALCHEMY_PAY_APP_SECRET
-    : 'wFNaNFP8YSgAm1dG',
+  appId: process.env.REACT_APP_ALCHEMY_PAY_APP_ID ? process.env.REACT_APP_ALCHEMY_PAY_APP_ID : '',
+  appSecret: process.env.REACT_APP_ALCHEMY_PAY_APP_SECRET ? process.env.REACT_APP_ALCHEMY_PAY_APP_SECRET : '',
   link: process.env.REACT_APP_ALCHEMY_PAY_URL ? process.env.REACT_APP_ALCHEMY_PAY_URL : `https://ramp.alchemypay.org`,
   network: 'BSC',
   crypto: 'USDT',
